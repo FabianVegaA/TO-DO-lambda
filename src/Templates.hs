ebac050790c83582462117bdad19108a13c4a13b
FabianVegaA <fabianveal@gmail.com>
2022-09-13 00:26:58 -0300

    fix(Templates): add button to makeTask

--- src/Templates.hs
+++ src/Templates.hs
@@ -3,14 +3,17 @@
 module Templates (makeTask, makeTaskList) where
 
 import Data.Text (Text)
-import Text.Blaze.Html5 (Html, div, h5, toHtml, (!))
-import Text.Blaze.Html5.Attributes (id)
+import Text.Blaze.Html5 (Html, button, div, h5, iframe, toHtml, (!))
+import Text.Blaze.Html5.Attributes (id, src)
 import Prelude hiding (div, id)
 
 makeTask :: Text -> Html
 makeTask task = div $ do
-    div ! id "task" $ mempty
-    div ! id "task" $ h5 $ toHtml task
+  div ! id "task" $ mempty
+  div ! id "task" $ do
+    h5 ! id "title-task" $ toHtml task
+  button ! id "remove-task" $ do
+    iframe ! src "icons/trash.svg" ! id "trash" $ mempty
 
 makeTaskList :: [Text] -> Html
 makeTaskList tasks =
